20b596ec9cfc017c946ea85cda39eed83d24d8ab
Raney <graney1@una.edu>
2022-09-01 13:19:29 -0500

    nodemon time

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const path = require('node:path')
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Node/Express/Heroku')
+  //res.send('Hello World from Node/Express/Heroku')
+
+  res.sendFile(
+    path.join(
+      __dirname,
+      "index.html"
+    )
+  );
+
 })
 
 app.listen(process.env.PORT || 3000,
